d42b2763720e124c608026c13df7b10bb0b581ea
Darin Kishore <darinkishore@protonmail.com>
2025-05-09 03:49:59 -0400

    feat: add document type definitions and interfaces

--- src/types/linear/index.ts
+++ src/types/linear/index.ts
@@ -1,9 +1,9 @@
-// Re-export all types from their respective files
 export * from './base';
-export * from './issue';
 export * from './comment';
-export * from './team';
-export * from './project';
 export * from './cycle';
-export * from './search';
+export * from './document';
 export * from './guards';
+export * from './issue';
+export * from './project';
+export * from './search';
+export * from './team';
